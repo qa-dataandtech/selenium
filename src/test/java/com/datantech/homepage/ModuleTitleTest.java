28b1dd80fcb572d6260880a33636b7ed8f311802
r3turnz3ro <khan.salauddin@hotmail.com>
2021-08-17 14:43:13 -0400

    new changes

--- src/test/java/com/datantech/homepage/ModuleTitleTest.java
+++ src/test/java/com/datantech/homepage/ModuleTitleTest.java
@@ -7,20 +7,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class ModuleTitleTest {
-    @Test
-    public void test1() {
-
-        // find the driver and use that
-        WebDriverManager.chromedriver().setup();
-
-        // step 2: create driver instance
-        WebDriver driver = new ChromeDriver();
-
-        // step 3: invoke get method
-        driver.get("https://www.datantech.com");
-
-
-        // close browser
-        driver.close();
-    }
+//    @Test
+//    public void test1() {
+//
+//        // find the driver and use that
+//        WebDriverManager.chromedriver().setup();
+//
+//        // step 2: create driver instance
+//        WebDriver driver = new ChromeDriver();
+//
+//        // step 3: invoke get method
+//        driver.get("https://www.datantech.com");
+//
+//
+//        // close browser
+//        driver.close();
+//    }
 }
